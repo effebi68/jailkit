7b6efcc7750af6e1f13ffdf3d599bf03fc31ffe2
Stricted <info@stricted.net>
2016-08-20 18:30:15 +0200

    fix undeclared variable error

--- src/jk_lib.c
+++ src/jk_lib.c
@@ -357,7 +357,7 @@ int jk_check_jail_owner (const char *jail, const char *user) {
 char *jk_extract_user (const char *path) {
 	char *user  = malloc(strlen(path) - strlen(JAIL_PREFIX) - 1);
 	if(user == NULL) {
-		syslog(LOG_ERR, "abort, failed to extract the username from jaildir '%s'", jail);
+		syslog(LOG_ERR, "abort, failed to extract the username from jaildir '%s'", path);
 		exit(17);
 	}
 	
